64c365c1302f35d5334bce7cec230404ec33cf12
Konstantin Suvorov <konsu@inbox.ru>
2018-05-09 18:14:39 +0300

    Добавьте файлы проекта.

---
+++ Pryamougol'nik/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pryamougol_nik
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
